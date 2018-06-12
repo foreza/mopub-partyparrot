6dc4e0b0af5669dcab2acb2922f6d917541711a0
Jason C <jforeza@gmail.com>
2018-06-11 17:46:04 -0700

    Add AerServ adapter files under com.mopub.mobil ads, be able to show interstitial + banner on application start

--- app/src/main/java/party/parrot/partyparrot/PartyMain.java
+++ app/src/main/java/party/parrot/partyparrot/PartyMain.java
@@ -18,9 +18,7 @@ import java.util.List;
 public class PartyMain extends AppCompatActivity implements MoPubView.BannerAdListener, MoPubInterstitial.InterstitialAdListener {
 
     private MoPubView moPubView;
-
     private MoPubInterstitial mInterstitial;
-
     static String log = "PARROT";
 
 
@@ -31,78 +29,46 @@ public class PartyMain extends AppCompatActivity implements MoPubView.BannerAdLi
 
        //  A list of rewarded video adapters to initialize
         List<String> networksToInit = new ArrayList();
-        // networksToInit.add("com.mopub.mobileads.AerServCustomEventBanner");
+        networksToInit.add("com.mopub.mobileads.AerServCustomEventBanner");
+        networksToInit.add("com.mopub.mobileads.AerServCustomEventInterstitial");
 
 
-        /*
-        - MoPub Plugin Android: 81aaafbeefd74cd787ca26041c6c8833 (has no impressions, not sure if works)
-        - MoPub Android Test Placement: 73bd31810f624db398332fa95e2d058d (serves a raw creative that we uploaded, works for sure)
-         */
-
-        SdkConfiguration sdkConfiguration = new SdkConfiguration.Builder("81aaafbeefd74cd787ca26041c6c8833")
+        // Party App ID: 6208244713bc4437a767f6aa8215bc29
+        SdkConfiguration sdkConfiguration = new SdkConfiguration.Builder("6208244713bc4437a767f6aa8215bc29")
                 .withNetworksToInit(networksToInit)
                 .build();
 
         MoPub.initializeSdk(this, sdkConfiguration, initSdkListener());
-
-
-        /*
-        - MocoSpace APP ID: 103156
-        -
-
-         */
-        AerServSdk.init(this, "103156"); // ibugs, should hopefully init. if not, use
-
-
-        /*
-        Placements to try..
-        1029840 - Publisher name: aerserv, Application name: My_Test_App, Placement name: iOSMopubPluginTest
-         */
-
-
-        /*
-        AerServ Plugin Interstitial (replacement) : (https://app.mopub.com/advertise/line_items/ee62d34418eb4863bd663c59c3ba4a14/edit/)
-        data optional: {"placement": "1029840", "appId": "1005911"}
-
-        will try changing it to something i can actually control / redirect to a customer so i can test
-
-                data optional: {"placement": "1007766", "appId": "103156"}
-
-
-
-         */
+        AerServSdk.init(this, "380000");
 
 
         moPubView = (MoPubView) findViewById(R.id.adview);
         moPubView.setBannerAdListener(this);
 
-        // This sample ad unit is from the MoPubSample aerserv application for an android banner: d73714cdd28d43bbbada62c6aaedfcc7
-        // Android AerServ Mediated Banner
-
-        /*
-
-        MoPub Plugin Android - Banner Ad: 9fe062ebcd89471cb09caa57c4add202
-
-         */
-
-        moPubView.setAdUnitId("9fe062ebcd89471cb09caa57c4add202"); // Enter your Ad Unit ID from www.mopub.com
-//        moPubView.loadAd();
+        // Party Banner: 549952a8447d4911b8d690c21b66abac
+         moPubView.setAdUnitId("549952a8447d4911b8d690c21b66abac");
 
 
-         // 6482ffa195a54c369e7bea3288919f4b - MoPubSample sample interstitial // Android AerServ Mediated Interstitial
-        // Loads an interstitial
-
-
-        /*
-        - MoPub Plugin Android, AerServ Fullscreen - a3fa142159ec45338c586765ec44c0f0
-        - MoPub Android Test Placement, AerServ Interstitial_2 - 5786ec14412744a4b0f9a706bbc0cd8d
-
-         */
-        mInterstitial = new MoPubInterstitial(this, "a3fa142159ec45338c586765ec44c0f0");
-        // Remember that "this" refers to your current activity.
-        mInterstitial.setInterstitialAdListener(this);
-        // mInterstitial.load();
+        // Party Interstitial: 2beb37597378451f85ef0bfba0cd7908\
+         mInterstitial = new MoPubInterstitial(this, "2beb37597378451f85ef0bfba0cd7908");
+         mInterstitial.setInterstitialAdListener(this);
 }
+
+    private SdkInitializationListener initSdkListener() {
+        return new SdkInitializationListener() {
+            @Override
+            public void onInitializationFinished() {
+
+                //  MoPub SDK initialized.
+                Log.d(log, "MoPub SDK initialized");
+
+                // Begin loading the interstitial, and load+show the banner
+                mInterstitial.load();
+                moPubView.loadAd();
+
+            }
+        };
+    }
 
 
 
@@ -135,25 +101,13 @@ public class PartyMain extends AppCompatActivity implements MoPubView.BannerAdLi
 
     }
 
-    // INTERSTITIALS
-
-
-//    // Defined by your application, indicating that you're ready to show an interstitial ad.
-//    void yourAppsShowInterstitialMethod() {
-//        if (mInterstitial.isReady()) {
-//            mInterstitial.show();
-//        } else {
-//            // Caching is likely already in progress if `isReady()` is false.
-//            // Avoid calling `load()` here and instead rely on the callbacks as suggested below.
-//        }
-//    }
 
     // InterstitialAdListener methods
+
     @Override
     public void onInterstitialLoaded(MoPubInterstitial interstitial) {
         // The interstitial has been cached and is ready to be shown.
         Log.d(log, "Interstitial loaded");
-
         mInterstitial.show();
     }
 
@@ -188,30 +142,13 @@ public class PartyMain extends AppCompatActivity implements MoPubView.BannerAdLi
 
 
 
-
-
-
     @Override
     protected void onDestroy() {
         mInterstitial.destroy();
         super.onDestroy();
     }
 
-    private SdkInitializationListener initSdkListener() {
-        return new SdkInitializationListener() {
-            @Override
-            public void onInitializationFinished() {
-           /* MoPub SDK initialized.
-           Check if you should show the consent dialog here, and make your ad requests. */
 
-                Log.d(log, "MoPub SDK init");
-
-                // mInterstitial.load();
-                moPubView.loadAd();
-
-            }
-        };
-    }
 
 
 }
